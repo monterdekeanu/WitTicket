b2d67bb88b9eb26bd8e15b8c6d9b4feba2488f7f
Keanu Anthony Monterde <keanuanthony.monterde.21@usjr.edu.ph>
2023-11-14 15:53:27 +0800

    Added: Search
    Functionalities Complete.
    
    To Do:
    Add User Profile
    Improve UI

--- WitTicket/View/Participant/DashboardParticipant.xaml.cs
+++ WitTicket/View/Participant/DashboardParticipant.xaml.cs
@@ -1,13 +1,19 @@
 namespace WitTicket.View.Participant;
+
+using CommunityToolkit.Maui.Core.Extensions;
 using CommunityToolkit.Maui.Views;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
 using WitTicket.Model;
+using WitTicket.Services;
 using WitTicket.View.Popups;
 using static OpenAI.ObjectModels.SharedModels.IOpenAiModels;
 
 public partial class DashboardParticipant : ContentPage
 {
     private ObservableCollection<EventModel> events = (new Services.Connection()).GetEventsList();
+    public ObservableCollection<EventModel> searchEvents { get; set; }
     public UserModel ActiveUser { get; set; }
 	public DashboardParticipant(UserModel activeUser)
     {
@@ -36,6 +42,8 @@ public partial class DashboardParticipant : ContentPage
                 cardView.Clicked += async (sender, e) => { await Navigation.PushAsync(new ParticipateEvent(eventModel,ActiveUser.AccountId)); };
                 if (eventModel.Images.Count > 0)
                 {
+                    Debug.Write("Image path: " + $@"{Path.Combine(Environment.GetEnvironmentVariable("DATABASE_EVENT_IMAGES", EnvironmentVariableTarget.Process), eventModel.Images[0])}");
+                    Debug.WriteLine(Path.Combine(Environment.GetEnvironmentVariable("DATABASE_EVENT_IMAGES", EnvironmentVariableTarget.Process), eventModel.Images[0]));
                     cardView.Source = ImageSource.FromFile($@"{Path.Combine(Environment.GetEnvironmentVariable("DATABASE_EVENT_IMAGES", EnvironmentVariableTarget.Process), eventModel.Images[0])}");
                 }
                 else
@@ -75,5 +83,17 @@ public partial class DashboardParticipant : ContentPage
         {
             return input.Substring(0, maxLength) + "...";
         }
+    }
+    private void OnSearchBarTextChange(object sender, EventArgs e)
+    {
+        if(((SearchBar)sender).Text == null || ((SearchBar)sender).Text == "")
+        {
+            searchEvents = null;
+            OnPropertyChanged(nameof(searchEvents));
+            return;
+        }
+        
+        searchEvents = events.Where(x => x.Name.ToLower().Contains(((SearchBar)sender).Text.ToLower())).ToObservableCollection<EventModel>();
+        OnPropertyChanged(nameof(searchEvents));
     }
 }
